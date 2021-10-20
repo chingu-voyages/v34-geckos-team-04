b23671addc87731c62d7bf62ec36d70e3607f3ea
PatrikG <patrik0gn@gmail.com>
2021-10-20 17:42:17 +0200

    Update: Remove handleTime function

--- src/components/EventInfo.js
+++ src/components/EventInfo.js
@@ -1,23 +1,11 @@
 import ToDoItem from './ToDoItem';
 import moment from 'moment';
 import { useState } from 'react';
+import handleTime from '../utils/handleTime';
 
 export default function EventInfo(props) {
   const { event } = props;
   const [todos, setTodos] = useState(event.todos);
-
-  function handleTime() {
-    const start = moment(event.start);
-    const end = moment(event.end);
-    // if event starts and ends on the same day
-    if (start.format('DDMM') === end.format('DDMM')) {
-      return start.format('DD/MM HH:mm') + ' - ' + end.format('HH:mm');
-    }
-    //if events start and ends on a different day
-    else {
-      return start.format('DD/MM HH:mm') + ' - ' + end.format('DD/MM HH:mm');
-    }
-  }
 
   function handleToDoDoneClick(todoClicked) {
     const doneTodos = todos.map((todo) => {
@@ -34,7 +22,7 @@ export default function EventInfo(props) {
     <li>
       <p>{event.name}</p>
       <p>{event.desc}</p>
-      <p>{handleTime()}</p>
+      <p>{handleTime(event.start, event.end)}</p>
       <p>{event.creator}</p>
       <ul>
         {todos.map((todo, index) => (
