3b399c269d7d4344a2107345fa494dc8524de968
PatrikG <patrik0gn@gmail.com>
2021-10-20 17:39:30 +0200

    Update: Remove handleTime function

--- src/components/EventItem.js
+++ src/components/EventItem.js
@@ -1,31 +1,12 @@
-import moment from 'moment';
+import handleTime from '../utils/handleTime';
 
 export default function EventItem(props) {
   const { event } = props;
 
-  // const startTime = moment(event.start).format('DD/MM HH:mm');
-  // const endTimeShort = moment(event.end).format('HH:mm');
-  // const endTimeLong = moment(event.end).format('DD/MM HH:mm')
-  // const startDay = moment(event.start).format('D');
-  // const endDay = moment(event.end).format('D');
-
-  function handleTime() {
-    const start = moment(event.start);
-    const end = moment(event.end);
-    // if event starts and ends on the same day
-    if (start.format('DDMM') === end.format('DDMM')) {
-      return start.format('DD/MM HH:mm') + ' - ' + end.format('HH:mm');
-    }
-    //if events start and ends on a different day
-    else {
-      return start.format('DD/MM HH:mm') + ' - ' + end.format('DD/MM HH:mm');
-    }
-  }
-
   return (
     <li>
       <p>{event.name}</p>
-      <p>{handleTime()}</p>
+      <p>{handleTime(event.start, event.end)}</p>
     </li>
   );
 }
