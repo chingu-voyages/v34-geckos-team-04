311d814a0476b08c880ea65d82921a0493fd6308
PatrikG <patrik0gn@gmail.com>
2021-10-20 15:47:19 +0200

    Add: Dummy event data

---
+++ src/utils/eventData.js
@@ -0,0 +1,57 @@
+const events = {
+  event1: {
+    name: 'EVENT-1',
+    desc: 'Description of Event-1',
+    start: '2021-10-28T09:00:00',
+    end: '2021-10-28T19:00:00',
+    creator: 'Emir',
+    todos: [
+      { name: 'TODO-1', status: false, assignees: 'Emir' },
+      { name: 'TODO-2', status: true, assignees: 'Patrik' },
+    ],
+  },
+  event2: {
+    name: 'EVENT-2',
+    desc: 'Description of Event-2',
+    start: '2021-10-29T09:00:00',
+    end: '2021-10-29T19:00:00',
+    creator: 'Patrik',
+    todos: [
+      { name: 'TODO-1', status: false, assignees: 'Emir' },
+      { name: 'TODO-2', status: true, assignees: 'Patrik' },
+    ],
+  },
+  event3: {
+    name: 'EVENT-3',
+    desc: 'Description of Event-3',
+    start: '2021-10-30T09:00:00',
+    end: '2021-10-30T19:00:00',
+    creator: 'Teri',
+    todos: [
+      { name: 'TODO-1', status: false, assignees: 'Emir' },
+      { name: 'TODO-2', status: true, assignees: 'Patrik' },
+    ],
+  },
+  event4: {
+    name: 'EVENT-4',
+    desc: 'Description of Event-4',
+    start: '2021-10-31T09:00:00',
+    end: '2021-10-31T19:00:00',
+    creator: 'Aya',
+    todos: [
+      { name: 'TODO-1', status: false, assignees: 'Emir' },
+      { name: 'TODO-2', status: true, assignees: 'Patrik' },
+    ],
+  },
+  event5: {
+    name: 'EVENT-5',
+    desc: 'Description of Event-5',
+    start: '2021-10-01T09:00:00',
+    end: '2021-10-01T19:00:00',
+    creator: 'Jihye',
+    todos: [
+      { name: 'TODO-1', status: false, assignees: 'Emir' },
+      { name: 'TODO-2', status: true, assignees: 'Patrik' },
+    ],
+  },
+};
