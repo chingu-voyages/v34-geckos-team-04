dae3d81f4a02be510fd632e76747361175ea3b85
PatrikG <patrik0gn@gmail.com>
2021-10-20 15:54:24 +0200

    Add: Route for Event page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import { UserContext } from './contexts/UserContext';
+import EventsPage from './pages/EventsPage';
 const App = () => {
   // Passed to UserContext.Provider to set and share user data
   // with different components
@@ -19,6 +20,9 @@ const App = () => {
         <UserContext.Provider value={{ userData, setUserData }}>
           <Route exact path='/'>
             {userData.loggedIn ? <Redirect to='/events' /> : <LoginPage />}
+          </Route>
+          <Route path='/events'>
+            <EventsPage />
           </Route>
         </UserContext.Provider>
       </Router>
