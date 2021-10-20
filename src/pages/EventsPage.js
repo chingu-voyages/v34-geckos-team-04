63ffbb72b2b9aa53279ce3a1c3896ad6c75188d1
PatrikG <patrik0gn@gmail.com>
2021-10-20 18:10:02 +0200

    Add: Logic for conditional rendering event info

--- src/pages/EventsPage.js
+++ src/pages/EventsPage.js
@@ -1,15 +1,26 @@
 import events from '../utils/eventData.js';
 import EventItem from '../components/EventItem.js';
+import EventInfo from '../components/EventInfo.js';
+import { useState } from 'react';
 
 export default function EventsPage() {
+  const [activeEvent, setActiveEvent] = useState(null);
+
   return (
     <div>
       <h1>EventsPage</h1>
       <ul>
         {events.map((event) => (
-          <EventItem key={event.id} event={event} />
+          <EventItem
+            key={event.id}
+            event={event}
+            onClick={() => setActiveEvent(event.id)}
+          />
         ))}
       </ul>
+      {activeEvent && (
+        <EventInfo event={events.find((event) => event.id === activeEvent)} />
+      )}
     </div>
   );
 }
