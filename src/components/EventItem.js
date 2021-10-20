fdeee6a42d7ae3d70c5c8b347ecae2696e1ddb94
PatrikG <patrik0gn@gmail.com>
2021-10-20 17:36:05 +0200

    Update: Remove not needed event informations

--- src/components/EventItem.js
+++ src/components/EventItem.js
@@ -1,10 +1,7 @@
-import ToDoItem from './ToDoItem';
 import moment from 'moment';
-import { useState } from 'react';
 
 export default function EventItem(props) {
   const { event } = props;
-  const [todos, setTodos] = useState(event.todos);
 
   // const startTime = moment(event.start).format('DD/MM HH:mm');
   // const endTimeShort = moment(event.end).format('HH:mm');
@@ -25,30 +22,10 @@ export default function EventItem(props) {
     }
   }
 
-  function handleToDoDoneClick(todoClicked) {
-    const doneTodos = todos.map((todo) => {
-      if (todo.id === todoClicked.id) {
-        return { ...todo, status: !todo.status };
-      } else {
-        return todo;
-      }
-    });
-    setTodos(doneTodos);
-  }
-
   return (
     <li>
       <p>{event.name}</p>
-      <p>{event.desc}</p>
       <p>{handleTime()}</p>
-      {/* <p>{startTime}</p>
-      <p>{event.end}</p> */}
-      <p>{event.creator}</p>
-      <ul>
-        {todos.map((todo, index) => (
-          <ToDoItem key={index} todo={todo} onDoneClick={handleToDoDoneClick} />
-        ))}
-      </ul>
     </li>
   );
 }
