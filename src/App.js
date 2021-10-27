3b8d1ee95601df37ee891d7a284b760a8df6644b
Emir AL <mythia@windowslive.com>
2021-10-27 13:46:58 +0300

    Add: Google auth logic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import { UserContext } from './contexts/UserContext';
+
 const App = () => {
   // Passed to UserContext.Provider to set and share user data
   // with different components
@@ -12,6 +13,31 @@ const App = () => {
     imageUrl: null,
   });
   console.log('userData', userData);
+
+  const { gapi } = window;
+  useEffect(() => {
+    if (!userData.loggedIn) {
+      gapi.load('client:auth2', () => {
+        gapi.auth2
+          .init({
+            client_id: process.env.REACT_APP_CLIENT_ID,
+          })
+          .then((googleAuth) => {
+            if (googleAuth.isSignedIn.get()) {
+              const googleUser = googleAuth.currentUser.get();
+              const basicProfile = googleUser.getBasicProfile();
+              setUserData({
+                loggedIn: true,
+                name: basicProfile.getName(),
+                email: basicProfile.getEmail(),
+                imageUrl: basicProfile.getImageUrl(),
+              });
+            }
+          })
+          .catch((err) => console.error('error:', err));
+      });
+    }
+  }, [gapi, userData]);
 
   return (
     <div>
