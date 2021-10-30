0a1bf9354bb81e242cec43df2b0b4d621f515502
Emir AL <mythia@windowslive.com>
2021-10-30 13:50:35 +0300

    Refactor: changed eventData state handling to use useReducer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useReducer } from 'react';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
 import { UserContext } from './contexts/UserContext';
 import { EventsContext } from './contexts/EventsContext';
 import EventsPage from './pages/EventsPage';
 import events from './utils/eventData';
+import eventsReducer from './utils/eventsReducer';
 
 const App = () => {
   // Passed to UserContext.Provider to set and share user data
@@ -17,13 +18,14 @@ const App = () => {
   });
   console.log('userData', userData);
 
-  const [eventData, setEventData] = useState(events);
+  const [eventData, dispatch] = useReducer(eventsReducer, events);
+  console.log('eventData', eventData);
 
   return (
     <div className='h-screen'>
       <Router>
         <UserContext.Provider value={{ userData, setUserData }}>
-          <EventsContext.Provider value={{ eventData, setEventData }}>
+          <EventsContext.Provider value={{ eventData, dispatch }}>
             <Route exact path='/'>
               {userData.loggedIn ? <Redirect to='/events' /> : <LoginPage />}
             </Route>
--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -3,28 +3,7 @@ import { EventsContext } from '../contexts/EventsContext';
 
 const ToDoItem = (props) => {
   const { todo, eventId } = props;
-  const { eventData, setEventData } = useContext(EventsContext);
-
-  const toggleTodo = (todoClicked) => {
-    const todos = eventData.find((e) => e.id === eventId).todos;
-    const doneTodos = todos.map((todo) => {
-      if (todo.id === todoClicked.id) {
-        return { ...todo, status: !todo.status };
-      } else {
-        return todo;
-      }
-    });
-    // updates EventsContext
-    setEventData(
-      eventData.map((e) => {
-        if (eventId === e.id) {
-          return { ...e, todos: doneTodos };
-        } else {
-          return e;
-        }
-      })
-    );
-  };
+  const { dispatch } = useContext(EventsContext);
 
   return (
     <li className='flex flex-row items-center mb-8 w-full'>
@@ -41,7 +20,9 @@ const ToDoItem = (props) => {
         type='checkbox'
         id={todo.id}
         className='form-checkbox rounded-full w-12 h-12 ml-2 text-blue-400 focus:ring-0'
-        onChange={() => toggleTodo(todo)}
+        onChange={() =>
+          dispatch({ type: 'toggleTodo', eventId, todoClicked: todo.id })
+        }
         checked={todo.status}
       />
       <label
