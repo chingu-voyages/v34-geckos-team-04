aacb01d37a0a8099925f517e4c53d2ce82b276a6
Emir AL <mythia@windowslive.com>
2021-10-30 12:53:02 +0300

    Refactor: moved todo toggle logic to ToDoItem

--- src/components/EventInfo.js
+++ src/components/EventInfo.js
@@ -6,7 +6,7 @@ import { useParams, Link } from 'react-router-dom';
 
 export default function EventInfo(props) {
   const { eventId } = useParams();
-  const { eventData, setEventData } = useContext(EventsContext);
+  const { eventData } = useContext(EventsContext);
   const event = eventData.find((e) => e.id === eventId);
 
   if (event === undefined) {
@@ -14,26 +14,6 @@ export default function EventInfo(props) {
   }
 
   const todos = event.todos;
-
-  function handleToDoDoneClick(todoClicked) {
-    const doneTodos = todos.map((todo) => {
-      if (todo.id === todoClicked.id) {
-        return { ...todo, status: !todo.status };
-      } else {
-        return todo;
-      }
-    });
-    // updates EventsContext
-    setEventData(
-      eventData.map((e) => {
-        if (event.id === e.id) {
-          return { ...e, todos: doneTodos };
-        } else {
-          return e;
-        }
-      })
-    );
-  }
 
   return (
     <li className='list-none'>
@@ -65,12 +45,7 @@ export default function EventInfo(props) {
       </div>
       <ul className='bg-red-400 text-black rounded-3xl flex flex-col items-center justify-center shadow-2xl py-6 px-6 max-w-full'>
         {todos.map((todo, index) => (
-          <ToDoItem
-            key={index}
-            todo={todo}
-            onDoneClick={handleToDoDoneClick}
-            prio='hidden'
-          />
+          <ToDoItem key={index} eventId={eventId} todo={todo} prio='hidden' />
         ))}
       </ul>
     </li>
--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,10 +1,30 @@
-export default function ToDoItem(props) {
-  const { todo } = props;
+import { useContext } from 'react';
+import { EventsContext } from '../contexts/EventsContext';
 
-  function handleDoneClick() {
-    console.log('ToDo done clicked');
-    props.onDoneClick(todo);
-  }
+const ToDoItem = (props) => {
+  const { todo, eventId } = props;
+  const { eventData, setEventData } = useContext(EventsContext);
+
+  const toggleTodo = (todoClicked) => {
+    const todos = eventData.find((e) => e.id === eventId).todos;
+    const doneTodos = todos.map((todo) => {
+      if (todo.id === todoClicked.id) {
+        return { ...todo, status: !todo.status };
+      } else {
+        return todo;
+      }
+    });
+    // updates EventsContext
+    setEventData(
+      eventData.map((e) => {
+        if (eventId === e.id) {
+          return { ...e, todos: doneTodos };
+        } else {
+          return e;
+        }
+      })
+    );
+  };
 
   return (
     <li className='flex flex-row items-center mb-8 w-full'>
@@ -21,7 +41,7 @@ export default function ToDoItem(props) {
         type='checkbox'
         id={todo.id}
         className='form-checkbox rounded-full w-12 h-12 ml-2 text-blue-400 focus:ring-0'
-        onChange={handleDoneClick}
+        onChange={() => toggleTodo(todo)}
         checked={todo.status}
       />
       <label
@@ -33,4 +53,5 @@ export default function ToDoItem(props) {
       </label>
     </li>
   );
-}
+};
+export default ToDoItem;
