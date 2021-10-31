d58761ef659c355d19b5d3ada9404d6376f18243
PatrikG <patrik0gn@gmail.com>
2021-10-31 03:24:16 +0100

    Add: EventSubMenu component to EventInfo

--- src/components/EventInfo.js
+++ src/components/EventInfo.js
@@ -3,6 +3,7 @@ import { useContext } from 'react';
 import handleTime from '../utils/handleTime';
 import { EventsContext } from '../contexts/EventsContext';
 import { useParams, Link } from 'react-router-dom';
+import EventSubMenu from './EventSubMenu';
 
 export default function EventInfo(props) {
   const { eventId } = useParams();
@@ -36,17 +37,18 @@ export default function EventInfo(props) {
   }
 
   return (
-    <li className='list-none'>
-      {/* Link is temporary until menu/header merged */}
-      {/* It adds ability to go back to events page on mobile screens */}
-      <Link
-        to='/events'
-        onClick={() => props.setActiveEvent(false)}
-        className='inline-block lg:hidden'
-      >
-        Back to events
-      </Link>
-      {/* <table className='border-separate'>
+    <div>
+      <li className='list-none'>
+        {/* Link is temporary until menu/header merged */}
+        {/* It adds ability to go back to events page on mobile screens */}
+        <Link
+          to='/events'
+          onClick={() => props.setActiveEvent(false)}
+          className='inline-block lg:hidden'
+        >
+          Back to events
+        </Link>
+        {/* <table className='border-separate'>
         <tbody>
           <tr>
             <th className='mr-10'>Name</th>
@@ -70,33 +72,39 @@ export default function EventInfo(props) {
           </tr>
         </tbody>
       </table> */}
-      <div>
-        <div className='flex flex-row justify-center'>
-          <p className='w-1/3'>Name:</p>
-          <p className='w-2/3'>{event.name}</p>
+        <div>
+          <div className='flex flex-row justify-center'>
+            <p className='w-1/3'>Name:</p>
+            <p className='w-2/3'>{event.name}</p>
+          </div>
+          <div className='flex flex-row justify-center'>
+            <p className='w-1/3'>Description:</p>
+            <p className='w-2/3'>{event.desc}</p>
+          </div>
+          <div className='flex flex-row justify-center'>
+            <p className='w-1/3'>Starts:</p>
+            <p className='w-2/3'>{handleTime(event.start)}</p>
+          </div>
+          <div className='flex flex-row justify-center'>
+            <p className='w-1/3'>Ends:</p>
+            <p className='w-2/3'>{handleTime(event.ends)}</p>
+          </div>
+          <div className='flex flex-row justify-center'>
+            <p className='w-1/3'>Creator:</p>
+            <p className='w-2/3'>{event.creator}</p>
+          </div>
         </div>
-        <div className='flex flex-row justify-center'>
-          <p className='w-1/3'>Description:</p>
-          <p className='w-2/3'>{event.desc}</p>
-        </div>
-        <div className='flex flex-row justify-center'>
-          <p className='w-1/3'>Starts:</p>
-          <p className='w-2/3'>{handleTime(event.start)}</p>
-        </div>
-        <div className='flex flex-row justify-center'>
-          <p className='w-1/3'>Ends:</p>
-          <p className='w-2/3'>{handleTime(event.ends)}</p>
-        </div>
-        <div className='flex flex-row justify-center'>
-          <p className='w-1/3'>Creator:</p>
-          <p className='w-2/3'>{event.creator}</p>
-        </div>
-      </div>
-      <ul>
-        {todos.map((todo, index) => (
-          <ToDoItem key={index} todo={todo} onDoneClick={handleToDoDoneClick} />
-        ))}
-      </ul>
-    </li>
+        <ul>
+          {todos.map((todo, index) => (
+            <ToDoItem
+              key={index}
+              todo={todo}
+              onDoneClick={handleToDoDoneClick}
+            />
+          ))}
+        </ul>
+      </li>
+      <EventSubMenu />
+    </div>
   );
 }
