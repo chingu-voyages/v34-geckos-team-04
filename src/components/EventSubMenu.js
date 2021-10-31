cb6a72549b004c9561c77441ff3fc8e0ba1c7464
PatrikG <patrik0gn@gmail.com>
2021-10-31 06:21:58 +0100

    Add: Conditional NavLink render for icons

--- src/components/EventSubMenu.js
+++ src/components/EventSubMenu.js
@@ -1,12 +1,31 @@
 import React from 'react';
 import { useState, useEffect, useLayoutEffect } from 'react';
 import { Icon } from '@iconify/react';
+import { NavLink } from 'react-router-dom';
 
 const eventSubMenu = [
-  { name: 'Availability', icon: 'fa-solid:user-clock' },
-  { name: 'Share', icon: 'fa-solid:share-alt' },
-  { name: 'Edit', icon: 'bx:bxs-edit' },
-  { name: 'Messages', icon: 'bx:bxs-message' },
+  {
+    name: 'Availability',
+    icon: 'fa-solid:user-clock',
+    isNavLink: true,
+    path: '/availability',
+  },
+  {
+    name: 'Share',
+    icon: 'fa-solid:share-alt',
+    isNavLink: false,
+  },
+  {
+    name: 'Edit',
+    icon: 'bx:bxs-edit',
+    isNavLink: false,
+  },
+  {
+    name: 'Messages',
+    icon: 'bx:bxs-message',
+    isNavLink: true,
+    path: '/messages',
+  },
 ];
 export default function EventSubMenu() {
   const [largeScreen, setLargeScreen] = useState(false);
@@ -32,8 +51,14 @@ export default function EventSubMenu() {
 
   const icon = eventSubMenu.map((icon) => {
     return (
-      <li key={icon.name} className='lg:flex lg:items-center'>
-        <Icon icon={icon.icon} color='#F0D2AC' width={36} height={36} />
+      <li key={icon.name} name={icon.name} className='lg:flex lg:items-center'>
+        {icon.isNavLink ? (
+          <NavLink to={icon.path}>
+            <Icon icon={icon.icon} color='#F0D2AC' width={36} height={36} />
+          </NavLink>
+        ) : (
+          <Icon icon={icon.icon} color='#F0D2AC' width={36} height={36} />
+        )}
       </li>
     );
   });
