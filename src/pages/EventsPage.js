9fa22974b3545545321941f84458ad5af2285097
Emir AL <mythia@windowslive.com>
2021-11-03 15:22:32 +0300

    Fix: removed Router from EventsPage to fix routing issues

--- src/pages/EventsPage.js
+++ src/pages/EventsPage.js
@@ -2,13 +2,7 @@ import EventItem from '../components/EventItem.js';
 import EventInfo from '../components/EventInfo.js';
 import { useState, useContext } from 'react';
 import { EventsContext } from '../contexts/EventsContext.js';
-import {
-  BrowserRouter as Router,
-  Link,
-  Route,
-  Switch,
-  Redirect,
-} from 'react-router-dom';
+import { Link, Route, Switch, Redirect } from 'react-router-dom';
 import ToDoList from '../components/ToDoList.js';
 
 export default function EventsPage() {
@@ -17,40 +11,38 @@ export default function EventsPage() {
 
   return (
     <div className='flex flex-row justify-around h-full lg:ml-56'>
-      <Router>
-        {!activeEvent && <Redirect to='/events' />}
-        <div
-          className={`lg:w-1/3 flex justify-center overflow-y-auto ${
-            activeEvent && 'hidden lg:flex'
-          }`}
-        >
-          <ul className='flex flex-col space-y-10 items-center w-screen'>
-            {events.map((event, index) => (
-              <Link key={event.id} to={`/events/${event.id}`}>
-                <EventItem event={event} onClick={() => setActiveEvent(true)} />
-              </Link>
-            ))}
-          </ul>
-        </div>
-        <div
-          className={`w-screen lg:w-2/3 flex flex-col bg-gray-50 border-l overflow-y-auto px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28 ${
-            !activeEvent && 'hidden lg:flex'
-          }`}
-        >
-          <Switch>
-            <Route path={`/events/:eventId`} exact>
-              {/* setActiveEvent prop is temporary until menu/header merged */}
-              <EventInfo setActiveEvent={setActiveEvent} />
-            </Route>
-            <Route path='/events/:eventId/todos'>
-              <ToDoList />
-            </Route>
-            <Route path='*'>
-              <p>Select an event to view</p>
-            </Route>
-          </Switch>
-        </div>
-      </Router>
+      {!activeEvent && <Redirect to='/events' />}
+      <div
+        className={`lg:w-1/3 flex justify-center overflow-y-auto ${
+          activeEvent && 'hidden lg:flex'
+        }`}
+      >
+        <ul className='flex flex-col space-y-10 items-center w-screen'>
+          {events.map((event, index) => (
+            <Link key={event.id} to={`/events/${event.id}`}>
+              <EventItem event={event} onClick={() => setActiveEvent(true)} />
+            </Link>
+          ))}
+        </ul>
+      </div>
+      <div
+        className={`w-screen lg:w-2/3 flex flex-col bg-gray-50 border-l overflow-y-auto px-8 sm:px-12 md:px-16 lg:px-20 xl:px-24 2xl:px-28 ${
+          !activeEvent && 'hidden lg:flex'
+        }`}
+      >
+        <Switch>
+          <Route path={`/events/:eventId`} exact>
+            {/* setActiveEvent prop is temporary until menu/header merged */}
+            <EventInfo setActiveEvent={setActiveEvent} />
+          </Route>
+          <Route path='/events/:eventId/todos'>
+            <ToDoList />
+          </Route>
+          <Route path='*'>
+            <p>Select an event to view</p>
+          </Route>
+        </Switch>
+      </div>
     </div>
   );
 }
