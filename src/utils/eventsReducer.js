b9b6fccb1a7d6387811c987c368d275c1e42f826
Emir AL <mythia@windowslive.com>
2021-11-09 13:48:02 +0300

    Update: eventsReducer to reduce code repetition

--- src/utils/eventsReducer.js
+++ src/utils/eventsReducer.js
@@ -1,11 +1,11 @@
 import { v4 as uuidv4 } from 'uuid';
 
 const eventsReducer = (state, action) => {
+  const todos = state.find((e) => e.id === action.eventId).todos;
+
   switch (action.type) {
     case 'toggleTodo':
-      //action: eventId todoId(todoClicked)
       // gets event todos from state
-      const todos = state.find((e) => e.id === action.eventId).todos;
       // creates a new updatedTodos array to toggle status
       const updatedTodos = todos.map((todo) =>
         todo.id === action.todoClicked
@@ -15,16 +15,14 @@ const eventsReducer = (state, action) => {
       return state.map((e) =>
         action.eventId === e.id ? { ...e, todos: updatedTodos } : e
       );
+
     case 'deleteTodo':
-      const todoArray = state.find((e) => e.id === action.eventId).todos;
-      const deletedTodos = todoArray.filter(
-        (todo) => todo.id !== action.todoId
-      );
+      const deletedTodos = todos.filter((todo) => todo.id !== action.todoId);
       return state.map((e) =>
         action.eventId === e.id ? { ...e, todos: deletedTodos } : e
       );
+
     case 'addTodo':
-      // {id, prio, name, status, assignees}
       const newTask = {
         id: uuidv4(),
         prio: action.prio === '' ? 'high' : action.prio,
@@ -32,11 +30,11 @@ const eventsReducer = (state, action) => {
         status: false,
         assignees: action.assignee,
       };
-      const addTodos = state.find((e) => e.id === action.eventId).todos;
-      addTodos.push(newTask);
-      return state.map((e) =>
-        e.id === action.eventId ? { ...e, todos: addTodos } : e
-      );
+
+      todos.push(newTask);
+
+      return state.map((e) => (e.id === action.eventId ? { ...e, todos } : e));
+
     case 'addNewEvent':
       const newEvent = {
         id: uuidv4(),
@@ -49,6 +47,7 @@ const eventsReducer = (state, action) => {
       };
 
       return [...state, newEvent];
+
     default:
       return state;
   }
