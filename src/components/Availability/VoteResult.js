a109344a2ee91026f4f7fa88a1cb32a5ea0be823
jihye kang <jihye59kang@gmail.com>
2021-11-11 04:11:18 +0000

    Add renderCustomDateCell function for showing time vote results

--- src/components/Availability/VoteResult.js
+++ src/components/Availability/VoteResult.js
@@ -8,12 +8,28 @@ const VoteResult = (props) => {
   const { eventData } = useContext(EventsContext);
   const availability = eventData.find((e) => e.id === eventId).availability;
   const schedule = availability.map(a => a.schedules);
+  const selectedTime = []
+  schedule.forEach(s1 => s1.forEach(s2 => 
+    selectedTime.push(s2)
+  ))
+  const votingCount = {}
+  selectedTime.forEach(e => votingCount[e] ? votingCount[e]++ : votingCount[e] = 1)
 
+  const renderCustomDateCell = (time, selected, innerRef) => {
+    <div style={{ textAlign: 'center' }} ref={innerRef}>
+      {selected ? `${votingCount[time]}` : ` `}
+    </div>
+  }
 
   return (
     <ScheduleSelector
       // showing selected time from all users
-      selection={schedule}
+      selection={selectedTime}
+      numDays={5}
+      minTime={8}
+      maxTime={22}
+      hourlyChunks={2}
+      renderDateCell={renderCustomDateCell}
     />
   );
 };
