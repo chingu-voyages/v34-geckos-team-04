11de04f240ec0b16ece5bcc3ba9b32120234b57e
Emir AL <mythia@windowslive.com>
2021-11-11 19:23:30 +0300

    Add: api.js to handle google api calls

---
+++ src/utils/api.js
@@ -0,0 +1,99 @@
+let gapi = window.gapi;
+const API_KEY = process.env.REACT_APP_API_KEY;
+const CLIENT_ID = process.env.REACT_APP_CLIENT_ID;
+const SCOPES = 'https://www.googleapis.com/auth/calendar.events';
+const DISCOVERY_DOCS = [
+  'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest',
+];
+
+export const initClient = (callback) => {
+  gapi.load('client:auth2', () => {
+    try {
+      gapi.client
+        .init({
+          apiKey: API_KEY,
+          clientId: CLIENT_ID,
+          discoveryDocs: DISCOVERY_DOCS,
+          scope: SCOPES,
+        })
+        .then(
+          () => {
+            if (typeof callback === 'function') {
+              callback(true);
+            }
+          },
+          (error) => {
+            console.log(error);
+          }
+        );
+    } catch (error) {
+      console.log(error);
+    }
+  });
+};
+
+export const checkSignInStatus = async () => {
+  try {
+    let status = await gapi.auth2.getAuthInstance().isSignedIn.get();
+    return status;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const signInToGoogle = async () => {
+  try {
+    let googleuser = await gapi.auth2
+      .getAuthInstance()
+      .signIn({ prompt: 'consent' });
+    if (googleuser) {
+      return true;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const signOutFromGoogle = () => {
+  try {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      auth2.disconnect();
+    });
+    return true;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const getSignedInUserEmail = async () => {
+  try {
+    let status = await checkSignInStatus();
+    if (status) {
+      var auth2 = gapi.auth2.getAuthInstance();
+      var profile = auth2.currentUser.get().getBasicProfile();
+      return profile.getEmail();
+    } else {
+      return null;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const publishTheCalendarEvent = (event) => {
+  try {
+    gapi.client.load('calendar', 'v3', () => {
+      var request = gapi.client.calendar.events.insert({
+        calendarId: 'primary',
+        resource: event,
+      });
+
+      request.execute(function (event) {
+        console.log('Event created: ' + event.htmlLink);
+      });
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
